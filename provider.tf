7dea08655534ef7ad2b2284bf7edf2e873525dc3
Gilk260 <gilk260@gmail.com>
2025-07-02 20:49:57 +0200

    feat(provider): add kubernetes and helm provider
    Signed-off-by: Gilk260 <gilk260@gmail.com>

--- provider.tf
+++ provider.tf
@@ -2,6 +2,14 @@ terraform {
   required_version = "= 1.9.1"
 
   required_providers {
+    helm = {
+      source = "hashicorp/helm"
+      version = "3.0.2"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.37.1"
+    }
     proxmox = {
       source = "bpg/proxmox"
       version = "0.78.2"
@@ -19,5 +27,16 @@ provider "proxmox" {
 
   ssh {
     agent = true
+  }
+}
+
+provider "kubernetes" {
+  host = "https://${var.cluster_address}:6443"
+  config_path = "./admin.conf"
+}
+
+provider "helm" {
+  kubernetes = {
+    config_path = "./admin.conf"
   }
 }
