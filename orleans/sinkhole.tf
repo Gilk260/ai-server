a2ce1a9250e0048d2efcaed713fd381378634e78
Gilk260 <gilk260@gmail.com>
2026-01-11 19:14:38 +0100

    feat(orleans: sinkhole): use IaC to store Adguard config
    Signed-off-by: Gilk260 <gilk260@gmail.com>

--- orleans/sinkhole.tf
+++ orleans/sinkhole.tf
@@ -4,6 +4,49 @@ locals {
   sinkhole_hostname = "sinkhole"
 }
 
+resource "adguard_config" "main" {
+  dhcp = {
+    enabled = true
+    interface = "eth0"
+    ipv4_settings = {
+      gateway_ip = "192.168.1.1"
+      lease_duration = 86400
+      range_start = "192.168.1.2"
+      range_end = "192.168.1.253"
+      subnet_mask = "255.255.255.0"
+    }
+  }
+
+  dns = {
+    upstream_dns = [
+      "https://dns.cloudflare.com/dns-query",
+      "https://dns.quad9.net/dns-query",
+    ]
+    upstream_mode = "parallel",
+    use_private_ptr_resolvers = true
+    local_ptr_upstreams = [
+      "192.168.1.1"
+    ]
+  }
+
+  querylog = {
+    interval = 24
+  }
+
+  depends_on = [
+    proxmox_virtual_environment_vm.dns_sinkhole
+  ]
+}
+
+resource "adguard_list_filter" "hagezi_threat_intelligence_feeds" {
+  name = "HaGeZi's Threat Intelligence Feeds"
+  url = "https://adguardteam.github.io/HostlistsRegistry/assets/filter_44.txt"
+
+  depends_on = [
+    proxmox_virtual_environment_vm.dns_sinkhole
+  ]
+}
+
 resource "proxmox_virtual_environment_vm" "dns_sinkhole" {
   name        = local.sinkhole_hostname
   description = "DNS Sinkhole (Pi-hole/AdGuard)"
@@ -17,12 +60,12 @@ resource "proxmox_virtual_environment_vm" "dns_sinkhole" {
 
   # DNS needs very little power
   cpu {
-    cores = 1
+    cores = 2
     type  = "host"
   }
 
   memory {
-    dedicated = 512
+    dedicated = 1024
   }
 
   disk {
@@ -87,11 +130,32 @@ resource "proxmox_virtual_environment_file" "sinkhole_config" {
       - unzip
       - tar
 
+    write_files:
+      - path: /tmp/AdGuardHome.yaml
+        owner: root:root
+        permissions: '0644'
+        content: |
+          http:
+            address: 0.0.0.0:80
+          users:
+            - name: admin
+              # This is "password" hashed.
+              password: ${var.adguard_password_hash}
+
     runcmd:
+      # port 53 conflict with AdGuardHome
+      - systemctl disable --now systemd-resolved
+      - rm -f /etc/resolv.conf
+      - echo "nameserver 1.1.1.1" > /etc/resolv.conf
+
       - systemctl enable qemu-guest-agent
       - systemctl start qemu-guest-agent
-      # Optional: Install AdGuard Home automatically (Simpler than Pi-hole for automation)
-      - curl -s -S -L https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh -s -- -v
+      - curl -s -S -L https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh -s -- -v -r
+
+      - systemctl stop AdGuardHome
+      - mv /tmp/AdGuardHome.yaml /opt/AdGuardHome/AdGuardHome.yaml
+      - chmod 644 /opt/AdGuardHome/AdGuardHome.yaml
+      - systemctl start AdGuardHome
     EOF
 
     file_name = "sinkhole.cloud-config.yaml"
