a91f00513817be1f740671aafc0da346eaeccd5a
Gilk260 <gilk260@gmail.com>
2026-01-26 08:44:39 +0100

    style(orleans: opnsense): factorize and use local variables
    Signed-off-by: Gilk260 <gilk260@gmail.com>

--- orleans/opnsense.tf
+++ orleans/opnsense.tf
@@ -1,73 +1,71 @@
+locals {
+  vpn_network    = "10.10.10.0/24"
+  lab_network    = "10.0.0.0/24"
+  wireguard_port = 51820
+}
+
 resource "wireguard_asymmetric_key" "lab_vpn" {
 }
 
 resource "opnsense_wireguard_client" "laptop" {
-  name             = "ManjaroLaptop"
-  public_key           = var.personal_key
-  tunnel_address   = ["10.10.10.2/32"]
+  name           = "ManjaroLaptop"
+  public_key     = chomp(file("./wireguard/personal_public.key"))
+  tunnel_address = ["10.10.10.2/32"]
 }
 
 resource "opnsense_wireguard_server" "lab_vpn" {
   name        = "LabVPN"
-  # The keys must be generated manually or via "random_password" resource
-  # Best practice: Use variables for sensitive keys
-  public_key      = wireguard_asymmetric_key.lab_vpn.public_key
-  private_key     = wireguard_asymmetric_key.lab_vpn.private_key
+  public_key  = wireguard_asymmetric_key.lab_vpn.public_key
+  private_key = wireguard_asymmetric_key.lab_vpn.private_key
 
   # Optional
-  port        = 51820
-  peers       = [opnsense_wireguard_client.laptop.id] # Links the peer below
+  port  = local.wireguard_port
+  peers = [opnsense_wireguard_client.laptop.id]
 }
 
 resource "opnsense_firewall_filter" "allow_wireguard_wan" {
   description = "Allow WireGuard Connect"
-  enabled     = true
-  sequence    = 1  # Order matters!
+  sequence    = 1 # Order matters!
 
-  # Schema: interface (Attributes) -> interface (Set of String)
   interface = {
     interface = ["wan"]
   }
 
-  # Schema: filter (Attributes)
   filter = {
-    action      = "pass"
-    direction   = "in"
-    protocol    = "UDP"
-    ip_protocol = "inet" # IPv4
+    action    = "pass"
+    direction = "in"
+    protocol  = "UDP"
 
-    # Schema: destination (Attributes) -> port (String)
+    # Optional
+    ip_protocol = "inet" # IPv4
     destination = {
-      port = "51820"
+      port = string(local.wireguard_port)
     }
   }
 }
 
 resource "opnsense_firewall_filter" "allow_wireguard_lan" {
   description = "Allow VPN to Lab"
-  enabled     = true
   sequence    = 2
 
-  # Note: "wireguard" is the automatic group created by the plugin.
-  # If this fails, try "opt1" or whatever interface ID OpnSense assigned.
+  # Need to enable Wireguard through the GUI, then assign interface wg0 to opt1
+  # something like that
   interface = {
     interface = ["opt1"]
   }
 
   filter = {
-    action      = "pass"
-    direction   = "in"
-    protocol    = "any"  # Allow TCP/UDP/ICMP
+    action    = "pass"
+    direction = "in"
+    protocol  = "any" # Allow TCP/UDP/ICMP
+
+    # Optional
     ip_protocol = "inet"
-
-    # Schema: source (Attributes) -> net (String)
     source = {
-      net = "10.10.10.0/24" # The VPN Network
+      net = local.vpn_network
     }
-
-    # Schema: destination (Attributes) -> net (String)
     destination = {
-      net = "10.0.0.0/24"   # The Lab Network
+      net = local.lab_network
     }
   }
 }
