51d75c4830eb26e7a06dd7de73f511b8362c3eba
Gilk260 <gilk260@gmail.com>
2026-01-29 20:47:14 +0100

    docs(orleans: network): specify private and lab networks
    Signed-off-by: Gilk260 <gilk260@gmail.com>

--- orleans/network.tf
+++ orleans/network.tf
@@ -1,8 +1,10 @@
+# Lab private network
 resource "proxmox_virtual_environment_network_linux_bridge" "vmbr1" {
   name      = "vmbr1"
   node_name = data.proxmox_virtual_environment_node.server.node_name
 }
 
+# Wifi radio link
 resource "proxmox_virtual_environment_network_linux_bridge" "vmbr2" {
   name      = "vmbr2"
   node_name = data.proxmox_virtual_environment_node.server.node_name
