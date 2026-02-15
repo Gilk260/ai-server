6deb319885b11fe0b56cd7739d4cc14a949d0868
Gilk260 <gilk260@gmail.com>
2026-02-15 17:18:43 +0100

    fix(orleans: network): add comments, and private IP range
    Signed-off-by: Gilk260 <gilk260@gmail.com>

--- orleans/network.tf
+++ orleans/network.tf
@@ -2,10 +2,13 @@
 resource "proxmox_virtual_environment_network_linux_bridge" "vmbr1" {
   name      = "vmbr1"
   node_name = data.proxmox_virtual_environment_node.server.node_name
+  address    = "10.0.0.2/24"
+  comment    = "Private Lan"
 }
 
 # Wifi radio link
 resource "proxmox_virtual_environment_network_linux_bridge" "vmbr2" {
   name      = "vmbr2"
   node_name = data.proxmox_virtual_environment_node.server.node_name
+  comment   = "WiFi-Uplink"
 }
