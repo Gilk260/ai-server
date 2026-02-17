299353d7acd8c7181a9b1eb2cb9ec2e8cb0c255d
Gilk260 <gilk260@gmail.com>
2026-02-17 21:04:09 +0100

    feat(paris: variables): claude code ip and windows11 template
    Signed-off-by: Gilk260 <gilk260@gmail.com>

--- paris/variables.tf
+++ paris/variables.tf
@@ -36,3 +36,16 @@ variable "worker_ip" {
   description = "Worker IP"
 }
 
+
+variable "claude_code_ip" {
+  type        = string
+  default     = "192.168.1.35"
+  description = "Worker IP"
+}
+
+variable "win11_template_vm_id" {
+  type        = number
+  description = "VM ID of the Windows 11 gaming template created by Packer"
+  # After running Packer, find the template VM ID in Proxmox and set this value
+  # in your terraform.tfvars file
+}
