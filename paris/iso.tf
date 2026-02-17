1b72e271722c8dbf284aca1243ac10a9072073a2
Gilk260 <gilk260@gmail.com>
2026-02-17 21:02:29 +0100

    feat(paris: iso): download ubuntu_24_noble
    Signed-off-by: Gilk260 <gilk260@gmail.com>

--- paris/iso.tf
+++ paris/iso.tf
@@ -5,6 +5,14 @@ resource "proxmox_virtual_environment_download_file" "latest_ubuntu_22_jammy_qco
   url          = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img" # Ubuntu Server LTS
 }
 
+resource "proxmox_virtual_environment_download_file" "ubuntu_24_noble" {
+  content_type = "iso"
+  datastore_id = "local"
+  node_name    = data.proxmox_virtual_environment_node.server.node_name
+  url          = "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
+  file_name    = "ubuntu-24.04-cloudimg-amd64.img"
+}
+
 # resource "proxmox_virtual_environment_download_file" "window_img" {
 #   content_type = "iso"
 #   datastore_id = "local"
