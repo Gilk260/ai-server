49050b9a605d3c69d36894baea0f0d9ed82d1a32
Gilk260 <gilk260@gmail.com>
2026-03-09 23:41:03 +0100

    fix(paris: windows11): remove
    Signed-off-by: Gilk260 <gilk260@gmail.com>

--- paris/windows11-packer/scripts/sysprep.ps1
+++
@@ -1,8 +0,0 @@
-Write-Host "Preparing sysprep..."
-# Remove temporary installers if any
-Remove-Item -Path "C:\Windows\Temp\scripts\*" -Force -Recurse -ErrorAction SilentlyContinue
-
-# Run Sysprep: OOBE + generalize so the image can be used as a template
-$sysprep = "$env:SystemRoot\System32\Sysprep\sysprep.exe"
-Start-Process -FilePath $sysprep -ArgumentList "/oobe /generalize /shutdown /quiet" -Wait
-# VM will shut down after sysprep; Packer will capture disk image
