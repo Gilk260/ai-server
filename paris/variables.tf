4c759c9ba4ce2f71ad6e2b00c9fd2bca243c021e
Gilk260 <gilk260@gmail.com>
2026-03-09 22:36:18 +0100

    fix(paris: variables): follow orleans archi
    Signed-off-by: Gilk260 <gilk260@gmail.com>

--- paris/variables.tf
+++ paris/variables.tf
@@ -1,3 +1,4 @@
+# --- Sensitive (from terraform.tfvars) ---
 variable "virtual_environment_username" {
   type      = string
   sensitive = true
@@ -18,34 +19,100 @@ variable "ssh_key" {
   sensitive = true
 }
 
-variable "control_plane_ip" {
-  type        = string
-  default     = "192.168.1.9"
-  description = "Control plane IP"
+# --- Infrastructure ---
+variable "cluster_name" {
+  type    = string
+  default = "paris"
 }
 
-variable "gaming_ip" {
+variable "cluster_domain" {
   type        = string
-  default     = "192.168.1.33"
-  description = "Gaming IP"
+  description = "Domain for cluster services (e.g., paris.g.recouvreux.fr)"
 }
 
-variable "worker_ip" {
-  type        = string
-  default     = "192.168.1.34"
-  description = "Worker IP"
+variable "k3s_version" {
+  type    = string
+  default = "v1.31.4+k3s1"
 }
 
-
-variable "claude_code_ip" {
-  type        = string
-  default     = "192.168.1.35"
-  description = "Worker IP"
+variable "cloud_vms" {
+  type = map(object({
+    vmid      = number
+    cores     = number
+    memory    = number
+    ip        = string
+    mgmt_ip   = optional(string)
+    disk_size = number
+    os_key    = string
+    packages  = optional(list(string), [])
+    runcmd    = optional(list(string), [])
+    k3s_role  = optional(string, "")
+    on_boot   = optional(bool, true)
+  }))
+  default = {
+    "k3s" = {
+      vmid      = 200
+      cores     = 4
+      memory    = 10240
+      ip        = "10.1.10.1"
+      mgmt_ip   = "192.168.1.10"
+      disk_size = 64
+      os_key    = "ubuntu-noble"
+      k3s_role  = "server"
+      on_boot   = true
+      packages  = ["nfs-common", "open-iscsi", "jq"]
+      runcmd    = []
+    }
+  }
 }
 
-variable "win11_template_vm_id" {
-  type        = number
-  description = "VM ID of the Windows 11 gaming template created by Packer"
-  # After running Packer, find the template VM ID in Proxmox and set this value
-  # in your terraform.tfvars file
+variable "opnsense_vm" {
+  type = object({
+    vmid      = number
+    cores     = number
+    memory    = number
+    disk_size = number
+  })
+  default = {
+    vmid      = 101
+    cores     = 2
+    memory    = 2048
+    disk_size = 16
+  }
+}
+
+variable "pihole_ct" {
+  type = object({
+    vmid   = number
+    cores  = number
+    memory = number
+    disk   = number
+    ip     = string
+  })
+  default = {
+    vmid   = 102
+    cores  = 1
+    memory = 512
+    disk   = 4
+    ip     = "10.1.0.2"
+  }
+}
+
+variable "gaming_vm" {
+  type = object({
+    vmid           = number
+    cores          = number
+    memory         = number
+    ip             = string
+    on_boot        = bool
+    template_vm_id = number
+  })
+  default = {
+    vmid           = 201
+    cores          = 4
+    memory         = 12288
+    ip             = "192.168.1.33"
+    on_boot        = false
+    template_vm_id = 100
+  }
 }
