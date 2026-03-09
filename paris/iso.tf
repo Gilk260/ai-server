1d342c86e56878618b11c48628c08ec4fb788d0b
Gilk260 <gilk260@gmail.com>
2026-03-09 22:36:57 +0100

    fix(paris: iso): clean images, ubuntu opnsense and debian
    Signed-off-by: Gilk260 <gilk260@gmail.com>

--- paris/iso.tf
+++ paris/iso.tf
@@ -1,11 +1,4 @@
-resource "proxmox_virtual_environment_download_file" "latest_ubuntu_22_jammy_qcow2_img" {
-  content_type = "iso"
-  datastore_id = "local"
-  node_name    = data.proxmox_virtual_environment_node.server.node_name
-  url          = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img" # Ubuntu Server LTS
-}
-
-resource "proxmox_virtual_environment_download_file" "ubuntu_24_noble" {
+resource "proxmox_virtual_environment_download_file" "ubuntu_noble" {
   content_type = "iso"
   datastore_id = "local"
   node_name    = data.proxmox_virtual_environment_node.server.node_name
@@ -13,9 +6,21 @@ resource "proxmox_virtual_environment_download_file" "ubuntu_24_noble" {
   file_name    = "ubuntu-24.04-cloudimg-amd64.img"
 }
 
-# resource "proxmox_virtual_environment_download_file" "window_img" {
-#   content_type = "iso"
-#   datastore_id = "local"
-#   node_name    = data.proxmox_virtual_environment_node.server.node_name
-#   url          = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img" # Ubuntu Server LTS
-# }
+# OPNsense ISO (verify URL at https://opnsense.org/download/)
+resource "proxmox_virtual_environment_download_file" "opnsense_iso" {
+  content_type            = "iso"
+  datastore_id            = "local"
+  node_name               = data.proxmox_virtual_environment_node.server.node_name
+  url                     = "https://pkg.opnsense.org/releases/26.1/OPNsense-26.1-dvd-amd64.iso.bz2"
+  file_name               = "OPNsense-26.1-dvd-amd64.iso"
+  decompression_algorithm = "bz2"
+}
+
+# Debian 12 LXC template for Pi-hole
+resource "proxmox_virtual_environment_download_file" "debian_lxc" {
+  content_type = "vztmpl"
+  datastore_id = "local"
+  node_name    = data.proxmox_virtual_environment_node.server.node_name
+  url          = "http://download.proxmox.com/images/system/debian-13-standard_13.1-2_amd64.tar.zst"
+  file_name    = "debian-13-standard_13.1-2_amd64.tar.zst"
+}
