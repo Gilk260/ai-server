007f2498aa22f969b08fb4e53b7069fc987a8a29
Gilk260 <gilk260@gmail.com>
2026-03-13 23:41:04 +0100

    feat(modules): add opnsense module (VM + firewall rules + WireGuard key)
    Signed-off-by: Gilk260 <gilk260@gmail.com>

---
+++ modules/opnsense/firewall.tf
@@ -0,0 +1,148 @@
+# --- Firewall Filter Rules ---
+
+resource "opnsense_firewall_filter" "allow_wireguard_wan" {
+  description = "Allow WireGuard Connect"
+  sequence    = 1
+
+  interface = {
+    interface = ["wan"]
+  }
+
+  filter = {
+    action      = "pass"
+    direction   = "in"
+    protocol    = "UDP"
+    ip_protocol = "inet"
+    destination = {
+      port = tostring(var.wireguard_port)
+    }
+  }
+}
+
+resource "opnsense_firewall_filter" "allow_lan_dns" {
+  description = "Allow LAN DNS"
+  sequence    = 2
+
+  interface = {
+    interface = ["lan"]
+  }
+
+  filter = {
+    action      = "pass"
+    direction   = "in"
+    protocol    = "UDP"
+    ip_protocol = "inet"
+    source = {
+      net = var.network_subnet
+    }
+    destination = {
+      port = "53"
+    }
+  }
+}
+
+resource "opnsense_firewall_filter" "allow_lan_https" {
+  description = "Allow LAN HTTPS"
+  sequence    = 3
+
+  interface = {
+    interface = ["lan"]
+  }
+
+  filter = {
+    action      = "pass"
+    direction   = "in"
+    protocol    = "TCP"
+    ip_protocol = "inet"
+    source = {
+      net = var.network_subnet
+    }
+    destination = {
+      port = "443"
+    }
+  }
+}
+
+resource "opnsense_firewall_filter" "allow_lan_http" {
+  description = "Allow LAN HTTP"
+  sequence    = 4
+
+  interface = {
+    interface = ["lan"]
+  }
+
+  filter = {
+    action      = "pass"
+    direction   = "in"
+    protocol    = "TCP"
+    ip_protocol = "inet"
+    source = {
+      net = var.network_subnet
+    }
+    destination = {
+      port = "80"
+    }
+  }
+}
+
+resource "opnsense_firewall_filter" "allow_vpn_to_lan" {
+  description = "Allow VPN to LAN"
+  sequence    = 5
+
+  interface = {
+    interface = ["opt1"]
+  }
+
+  filter = {
+    action      = "pass"
+    direction   = "in"
+    protocol    = "any"
+    ip_protocol = "inet"
+    source = {
+      net = var.wireguard_subnet
+    }
+    destination = {
+      net = var.network_subnet
+    }
+  }
+}
+
+resource "opnsense_firewall_filter" "allow_wan_opnsense_api" {
+  description = "Allow WAN to OPNsense API"
+  sequence    = 6
+
+  interface = {
+    interface = ["wan"]
+  }
+
+  filter = {
+    action      = "pass"
+    direction   = "in"
+    protocol    = "TCP"
+    ip_protocol = "inet"
+    source = {
+      net = "192.168.1.0/24"
+    }
+    destination = {
+      port = "443"
+    }
+  }
+}
+
+# --- NAT ---
+
+resource "opnsense_firewall_nat" "lan_to_wan" {
+  description = "NAT LAN to WAN"
+  interface   = "wan"
+  protocol    = "any"
+  ip_protocol = "inet"
+  sequence    = 1
+
+  source = {
+    net = var.network_subnet
+  }
+
+  target = {
+    ip = "wanip"
+  }
+}
---
+++ modules/opnsense/variables.tf
@@ -0,0 +1,45 @@
+variable "node_name" {
+  type = string
+}
+
+variable "opnsense_vm" {
+  type = object({
+    vmid      = number
+    cores     = number
+    memory    = number
+    disk_size = number
+  })
+}
+
+variable "gateway_ip" {
+  type = string
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "network_subnet" {
+  type        = string
+  description = "Private LAN CIDR (e.g., 10.0.0.0/16)"
+}
+
+variable "wireguard_subnet" {
+  type = string
+}
+
+variable "opnsense_iso_file_id" {
+  type        = string
+  description = "Proxmox file ID of the OPNsense ISO"
+}
+
+variable "lan_bridge" {
+  type        = string
+  description = "LAN bridge name (e.g., vmbr1)"
+  default     = "vmbr1"
+}
+
+variable "wireguard_port" {
+  type    = number
+  default = 51820
+}
