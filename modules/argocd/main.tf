1cf2f2ad9bbed973995c42e086b7f8d633f2f267
Gilk260 <gilk260@gmail.com>
2026-03-13 23:54:33 +0100

    feat: add argocd module with cluster-specific Helm overrides
    Signed-off-by: Gilk260 <gilk260@gmail.com>

---
+++ modules/argocd/main.tf
@@ -0,0 +1,47 @@
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  version          = "9.1.0"
+  namespace        = "argocd"
+  create_namespace = true
+  wait             = true
+  timeout          = 600
+
+  values = [
+    file(var.helm_values_path)
+  ]
+
+  # Dynamic cluster-specific overrides
+  set {
+    name  = "global.domain"
+    value = "argocd.${var.cluster_domain}"
+  }
+
+  set {
+    name  = "server.ingress.hostname"
+    value = "argocd.${var.cluster_domain}"
+  }
+
+  set {
+    name  = "configs.clusterCredentials.in-cluster.name"
+    value = var.cluster_name
+  }
+
+  set {
+    name  = "configs.clusterCredentials.in-cluster.labels.cluster-name"
+    value = var.cluster_name
+  }
+
+  set {
+    name  = "configs.clusterCredentials.in-cluster.labels.env"
+    value = var.cluster_name
+  }
+
+  set_sensitive = [
+    {
+      name  = "configs.repositories.infra-gitops.githubAppPrivateKey"
+      value = file(var.private_key_path)
+    },
+  ]
+}
