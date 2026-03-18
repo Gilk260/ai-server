a9892d0e89ab303aa5e1ddcf43b2489787e56f7d
Gilk260 <gilk260@gmail.com>
2026-03-19 00:04:59 +0100

    fix(modules: cloud_vms: kubeconfig): can access kubeconfig only through VPN tunnel
    Signed-off-by: Gilk260 <gilk260@gmail.com>

--- modules/cloud_vms/kubeconfig.tf
+++ modules/cloud_vms/kubeconfig.tf
@@ -1,6 +1,7 @@
 locals {
-  k3s_fetch_ip   = coalesce(var.k3s_mgmt_ip, var.k3s_server_ip)
+  k3s_ssh_target = coalesce(var.k3s_mgmt_ip, var.k3s_server_ip)
   k3s_ssh_prefix = var.k3s_mgmt_ip != null ? "" : "-J root@${var.proxmox_ip}"
+  k3s_api_ip     = var.k3s_server_ip
 }
 
 resource "terraform_data" "k3s_kubeconfig" {
@@ -18,9 +19,9 @@ resource "terraform_data" "k3s_kubeconfig" {
       TMPFILE=$(mktemp)
 
       # Fetch kubeconfig from K3s server
-      ssh -o StrictHostKeyChecking=no ${local.k3s_ssh_prefix} ubuntu@${local.k3s_fetch_ip} \
+      ssh -o StrictHostKeyChecking=no ${local.k3s_ssh_prefix} ubuntu@${local.k3s_ssh_target} \
           'sudo cat /etc/rancher/k3s/k3s.yaml' \
-        | sed "s|127.0.0.1|${local.k3s_fetch_ip}|g" \
+        | sed "s|127.0.0.1|${local.k3s_api_ip}|g" \
         | sed "s|default|${var.cluster_name}|g" \
         > "$TMPFILE"
 
