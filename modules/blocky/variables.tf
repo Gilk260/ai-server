d8d969673f6fde883ee735c8dd06abdae6d068cf
Gilk260 <gilk260@gmail.com>
2026-04-04 13:26:48 +0200

    feat(modules): create blocky module
    Signed-off-by: Gilk260 <gilk260@gmail.com>

---
+++ modules/blocky/variables.tf
@@ -0,0 +1,60 @@
+variable "node_name" {
+  type = string
+}
+
+variable "blocky_ct" {
+  type = object({
+    vmid   = number
+    cores  = number
+    memory = number
+    disk   = number
+    ip     = string
+  })
+}
+
+variable "gateway_ip" {
+  type = string
+}
+
+variable "network_subnet" {
+  type        = string
+  description = "Private network CIDR (for IP address mask)"
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "cluster_domain" {
+  type = string
+}
+
+variable "k3s_server_ip" {
+  type        = string
+  description = "K3s server IP for wildcard DNS resolution"
+}
+
+variable "ssh_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "proxmox_ip" {
+  type        = string
+  description = "Proxmox host IP for SSH provisioner (pct exec)"
+}
+
+variable "lxc_template_file_id" {
+  type        = string
+  description = "Proxmox file ID of the Debian LXC template"
+}
+
+variable "lan_bridge" {
+  type    = string
+  default = "vmbr1"
+}
+
+variable "vm_datastore_id" {
+  type    = string
+  default = "local-lvm"
+}
